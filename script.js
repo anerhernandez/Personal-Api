2cf90f61aea1d710234de98955b476dacb25c312
anerhernandez <anerhernandezl@gmail.com>
2025-02-08 15:59:33 +0000

    Shows in console filtered weapons save in localstorage

--- script.js
+++ script.js
@@ -11,24 +11,32 @@ function append(parent, child) {
 function filterbyweapontype(data, type){
     return data.filter((element) => element.type == type);
 }
-
-fetch(API)
+function buscar(){
+    console.log(filterbyweapontype(JSON.parse(localStorage.getItem("weapons")), document.getElementById("tipo").value))
+}
+if (localStorage.getItem("weapons") == null) {
+    document.getElementById("loading").textContent = "Loading API"
+    fetch(API)
     .then((resp) => resp.json())
     .then(function (data) {
-
-        const allweapons = data
-
-        const savedweapons = new Map(
-            [
-            ["long-swords", filterbyweapontype(allweapons, "long-sword")],
-            ["great-swords", filterbyweapontype(allweapons, "great-sword")],
-            ["charge-blades", filterbyweapontype(allweapons, "charge-blade")],
-        ]);
-        let contenedor = createNode("div")
-
-        contenedor.textnode= "sample text"
-        append(document.getElementById("id_div"), contenedor)
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+            document.getElementById("loading").textContent = "API loaded"
+    
+            
+            const allweapons = data
+    
+            // const savedweapons = new Map(
+            //     [
+            //     ["long-swords", filterbyweapontype(allweapons, "long-sword")],
+            //     ["great-swords", filterbyweapontype(allweapons, "great-sword")],
+            //     ["charge-blades", filterbyweapontype(allweapons, "charge-blade")],
+            // ]);
+            localStorage.setItem("weapons", JSON.stringify(allweapons))
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}else{
+    document.getElementById("loading").textContent = "API loaded"
+    // let weapons = JSON.parse (localStorage.getItem("weapons"))
+    // console.log(weapons)
+}
