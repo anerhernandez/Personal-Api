c0647487de568ccb08d01c28d1f1ffe8bb7f8ab2
anerhernandez <anerhernandezl@gmail.com>
2025-02-09 22:38:27 +0000

    filter weapon and shown in card working

--- script.js
+++ script.js
@@ -12,16 +12,88 @@ function filterbyweapontype(data, type){
     return data.filter((element) => element.type == type);
 }
 function buscar(){
-    console.log(filterbyweapontype(JSON.parse(localStorage.getItem("weapons")), document.getElementById("tipo").value))
+    document.getElementById("notfound").innerHTML=""
+    document.getElementById("id_div").innerHTML=""
+    return filterbyweapontype(JSON.parse(localStorage.getItem("weapons")), document.getElementById("tipo").value)
 }
-if (localStorage.getItem("weapons") == null) {
+
+function createcard(element){
+    let card = createNode("div")
+    card.setAttribute("id", element.id)
+    card.classList.add(
+        "border",
+        "border-gray-600",
+        "rounded",
+    )
+    
+    if (element.assets == "" || element.assets == null || element.assets.image == null ) {
+        let nophoto = createNode("li")
+        nophoto.textContent = "No se han encontrado imagenes de este arma: " + element.name
+        append(card, nophoto)
+        append(card, createNode("hr"))
+    } else {
+        let img = createNode("img")
+        img.setAttribute("src", element.assets.image) 
+        append(card, img)
+        append(card, createNode("hr"))
+    }
+    let lista = createNode("ul")
+    append(card, lista)
+
+
+    let nombre = createNode("li")
+    nombre.textContent = "Nombre: " + element.name
+    append(lista,nombre)
+
+
+    if (element.elements[0] == "" || element.elements[0] == null || element.elements[0].type == null ) {
+        let elemento = createNode("li")
+        elemento.textContent = "Elemento: Sin elemento"
+
+        append(lista, elemento)
+    } else {
+        let elemento = createNode("li")
+        elemento.textContent = "Elemento: " + element.elements[0].type
+
+        append(lista, elemento)
+
+        let elementodmg = createNode("li")
+        elementodmg.textContent = " Daño de elemento: " + element.elements[0].damage
+        append(lista, elementodmg)
+    }
+    append(document.getElementById("cards"), card)
+}
+
+
+function renderweapons(){
+    let weaponsSearched = buscar()
+    if (weaponsSearched == null || weaponsSearched == "") {
+        document.getElementById("notfound").textContent = "No se encontraron armas de tipo: " + document.getElementById("tipo").value 
+    } else {
+        console.log(weaponsSearched)
+        let cards = createNode("div")
+        cards.setAttribute('id', "cards")
+        cards.classList.add(
+            "grid",
+            "grid-cols-1",
+            "md:grid-cols-2",
+            "lg:grid-cols-3",
+            "gap-5"
+        )
+        
+        append(document.getElementById("id_div"), cards)
+        weaponsSearched.forEach(element => {
+            createcard(element)
+        });
+    }
+}
+
+if (localStorage.getItem("weapons") === null) {
     document.getElementById("loading").textContent = "Loading API"
     fetch(API)
     .then((resp) => resp.json())
     .then(function (data) {
             document.getElementById("loading").textContent = "API loaded"
-    
-            
             const allweapons = data
     
             // const savedweapons = new Map(
@@ -37,6 +109,4 @@ if (localStorage.getItem("weapons") == null) {
         });
 }else{
     document.getElementById("loading").textContent = "API loaded"
-    // let weapons = JSON.parse (localStorage.getItem("weapons"))
-    // console.log(weapons)
 }
